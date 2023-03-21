774415b7eeec724510a5c559dda485bb23253532
fe2o0o <ibrahimfe2o0o@gmail.com>
2023-03-21 08:23:50 -0700

    absloute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - to computes the absolute value of an integer.
+ * @n : the int to check
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	int res = abs(n);
+
+	return (res);
+}
