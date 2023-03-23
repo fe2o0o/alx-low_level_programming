3ba4e2ff8656e171a6dba04e19429f1e66ea82ad
fe2o0o <ibrahimfe2o0o@gmail.com>
2023-03-23 07:47:50 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - to get multipli 2 number
+ * @a: first number
+ * @b: second number
+ * Return: multipli a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
