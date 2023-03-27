bd12f58ae9763189c6df045672fd8bf27f03be6e
fe2o0o <ibrahimfe2o0o@gmail.com>
2023-03-27 07:57:25 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - function to find length of string
+ * @s : array of character
+ * Return:length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
