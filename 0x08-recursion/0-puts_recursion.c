131dcc210fa8fd01d1bd2bcf168b0f796437ce99
fe2o0o <ibrahimfe2o0o@gmail.com>
2023-04-06 06:32:48 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts_recursion - print collection of character
+ *@s: character will be print
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
